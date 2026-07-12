bf9e1e87b6b49242e53e8e49023875906305bd69
Aquaculturebd <abc@gmail.com>
2026-07-12 18:00:45 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,7 @@
-
-
-
-
-
-
-
-
-
-
-
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() {
   runApp(const BiotaHerbalApp());
-}
-
-class BiotaHerbalApp extends StatelessWidget {
-  const BiotaHerbalApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Biota Herbal Intelligence',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Biota Herbal Intelligence'),
-        ),
-        body: const Center(
-          child: Text('Welcome to Biota Herbal Intelligence'),
-        ),
-      ),
-    );
-  }
 }
 
